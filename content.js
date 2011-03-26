ef1808141e0d029c3a949cb9b80842329bc8e266
Kevin Wallace <kevin@pentabarf.net>
2011-03-26 13:17:30 -0700

    Try to detect if Chrome is lying to us about document.lastModified.

--- content.js
+++ content.js
@@ -1,7 +1,14 @@
 if(document.lastModified) {
-    var lastModified = new Date(document.lastModified + " GMT").toString();
-    chrome.extension.sendRequest({
-        msg: "lastModified",
-        lastModified: lastModified
-    });
+    var lastModified = new Date(document.lastModified + " GMT");
+    var now = new Date();
+    if (now.getTime() - lastModified.getTime() > 1000) {
+      // This is hacky, but there doesn't seem to be any way to tell if the
+      // date returned from document.lastModified is based on a Last-Modified
+      // header, or made up on the spot by Chrome.  We'll assume that if it's
+      // within 1 second of now, Chrome is lying to us.
+      chrome.extension.sendRequest({
+          msg: "lastModified",
+          lastModified: lastModified.toString()
+      });
+    }
 }
